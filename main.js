38e42cd8faec79d32c6724f01cd3576a0f03af60
Jared Henry Oviatt <jaredhenryoviatt@gmail.com>
2015-02-10 21:02:29 -0800

    committing all jho online files

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+   $('.tableOption').mouseenter(function() {
+       $(this).animate({
+           height: '+=5px'
+       });
+   });
+   $('.tableOption').mouseleave(function() {
+       $(this).animate({
+           height: '-=5px'
+       });
+   });
+});
