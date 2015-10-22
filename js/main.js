6a2997c73fc7bc4c5a04d323d954b60bd4a4ff57
Jared Henry Oviatt <jaredhenryoviatt@gmail.com>
2015-10-21 20:22:29 -0700

    reformatting

--- js/main.js
+++ js/main.js
@@ -1,21 +1,12 @@
 $(document).ready(function() {
-   $('.tableOption').mouseenter(function() {
-       $(this).animate({
-           height: '+=5px'
-       });
-   });
-   $('.tableOption').mouseleave(function() {
-       $(this).animate({
-           height: '-=5px'
-       });
-   });
+  $('.tableOption').mouseenter(function() {
+    $(this).animate({
+      height: '+=5px'
+    });
+  });
+  $('.tableOption').mouseleave(function() {
+    $(this).animate({
+      height: '-=5px'
+    });
+  });
 });
-/*
-$(document).ready(function() {
-   $('.tableOption').mouseenter(function() {
-       $(this).width('110%');
-   });
-   $('.tableOption').mouseleave(function() {
-       $(this).width('100%');
-   });
-});*/
